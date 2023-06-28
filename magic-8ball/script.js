fb31c4e175b9012606c85dd625b312c142b889bc
Patrick <pb760@uowmail.edu.au>
2023-06-28 14:45:55 +1000

    css and javascript for magic 8 ball updated

--- magic-8ball/script.js
+++ magic-8ball/script.js
@@ -3,21 +3,63 @@ div = document.createElement("div");
 div.setAttribute("class", "container");
 document.body.appendChild(div);
 
-myFunctionList = document.createElement("div");
-myFunctionList.setAttribute("class", "cmd-list clearfix");
-div.appendChild(myFunctionList);
+// heading
+h1 = document.createElement("h1");
+h1.setAttribute("class", "heading");
+h1.textContent = "Please Ask a Question";
+div.appendChild(h1);
 
-//   br = document.createElement("br");
-//   myFunctionList.appendChild(br);
+// input
+input = document.createElement("input");
+input.setAttribute("type", "text");
+input.setAttribute("class", "user-input");
+input.setAttribute("placeholder", "Question");
+input.setAttribute("autofocus", "");
 
-// div
-myBlock = document.createElement("div");
-myBlock.setAttribute("class", "myBlock");
-div.appendChild(myBlock);
-let currentPos = myBlock.getBoundingClientRect();
+div.appendChild(input);
 
-// Heading
-p = document.createElement("p");
-p.setAttribute("class", "block-txt");
-p.textContent = "Block";
-myBlock.append(p);
+// button
+button = document.createElement("button");
+button.setAttribute("type", "submit");
+button.setAttribute("class", "btn btn-ask");
+button.innerHTML = "Ask";
+div.appendChild(button);
+
+// display output
+divOutput = document.createElement("div");
+divOutput.setAttribute("class", "output");
+div.appendChild(divOutput);
+
+// result
+resultP = document.createElement("p");
+resultP.setAttribute("class", "result");
+divOutput.appendChild(resultP);
+
+// select elements
+const ansArray = [
+  "Lorem ipsum dolor, sit amet",
+  "consectetur adipisicing elit. Quaerat eum enim blanditiis! Tempore, nisi, sunt ipsum et veniam cum aperiam magni perspiciatis",
+  "consequuntur optio sed, ducimus sequi. Rem dolores doloremque",
+  "possimus nam nesciunt qui eum. ",
+  "Dolore quisquam ducimus doloribus omnis",
+];
+const message = document.querySelector(".heading");
+const question = document.querySelector(".user-input");
+const btnEl = document.querySelector(".btn-ask");
+const resultEl = document.querySelector(".result");
+const keyEnter = 13;
+
+btnEl.addEventListener("click", (e) => {
+  
+    console.log(question.value);
+
+    let result = Math.floor(Math.random() * ansArray.length);
+    console.log(result);
+    if (question.value !== "") {
+      resultEl.innerText = question.value + " " + ansArray[result];
+      question.value = "";
+    } else {
+      resultEl.innerText = "Pleae enter a question...";
+    }
+  
+});
